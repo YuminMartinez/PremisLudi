f18505f795e7240e8b96ee23046cf4617f6e79b1
Julsi06 <julia.sanchez@enti.cat>
2025-10-15 21:26:19 +0200

    transition init screen, level selector, level 1 done

--- Assets/project/Scripts/ChangeScenes.cs
+++ Assets/project/Scripts/ChangeScenes.cs
@@ -5,8 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScenes : MonoBehaviour
 {
-    public void GoToGame()
+    public void GoToLevelSelector()
     {
-        SceneManager.LoadScene("GameScene");
+        SceneManager.LoadScene("LevelSelector");
+    }
+
+    public void GoToLevel1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void GoToLevel2()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+
+    public void GoToLevel3()
+    {
+        SceneManager.LoadScene("Level3");
+    }
+
+    public void GoToLevel4()
+    {
+        SceneManager.LoadScene("Level4");
+    }
+
+    public void GoToLevel5()
+    {
+        SceneManager.LoadScene("Level5");
     }
 }
