4a8daa384e2874dc50ac9858eed34c298d555faa
Yumin Martinez Perez <yumin.martinez@enti.cat>
2025-10-29 13:40:45 +0100

    candasod

--- Assets/project/Scripts/LockLevelsImage.cs
+++ Assets/project/Scripts/LockLevelsImage.cs
@@ -1,38 +1,34 @@
-using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class LockLevelsImage : MonoBehaviour
 {
-    Timer timer;
     [SerializeField] private Image lockImage2;
     [SerializeField] private Image lockImage3;
     [SerializeField] private Image lockImage4;
     [SerializeField] private Image lockImage5;
+
     private void Awake()
     {
+        // Siempre activar todas las imágenes al empezar
         lockImage2.gameObject.SetActive(true);
         lockImage3.gameObject.SetActive(true);
         lockImage4.gameObject.SetActive(true);
         lockImage5.gameObject.SetActive(true);
-        timer = GetComponent<Timer>();
     }
 
-    private void Update()
+    private void Start()
     {
+       
         CheckImageLock();
     }
 
     private void CheckImageLock()
     {
-        if (timer.level2 == true)
-            lockImage2.gameObject.SetActive(false);
-        if (timer.level3 == true)
-            lockImage3.gameObject.SetActive(false);
-        if (timer.level4 == true)
-            lockImage4.gameObject.SetActive(false);
-        if (lockImage5 == true)
-            lockImage5.gameObject.SetActive(false);
+        // Oculta el candado si el nivel anterior fue completado
+        if (PlayerPrefs.GetInt("Level1Completed", 0) == 1) lockImage2.gameObject.SetActive(false);
+        if (PlayerPrefs.GetInt("Level2Completed", 0) == 1) lockImage3.gameObject.SetActive(false);
+        if (PlayerPrefs.GetInt("Level3Completed", 0) == 1) lockImage4.gameObject.SetActive(false);
+        if (PlayerPrefs.GetInt("Level4Completed", 0) == 1) lockImage5.gameObject.SetActive(false);
     }
-
 }
